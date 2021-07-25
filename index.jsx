636ddc9653e075607488737c7969d9d5105f3cd2
CheoR <cheo.roman+github@gmail.com>
2021-07-25 07:37:07 -0500

    build: refact routes to use one endpoint.

--- index.jsx
+++ index.jsx
@@ -9,11 +9,6 @@ app.use(express.static('public'));
 app.use('/images', express.static('images'));
 
 // routes
-app.get('/', (req, res) => {
-	// return res.send(`GET /: request on port ${PORT}`);
-	return res.json(data);
-});
-
 app.get('/item/:id', (req, res, next) => {
 	const id = Number(req.params.id);
 	console.log(`UserId: ${id}`);
@@ -24,17 +19,31 @@ app.get('/item/:id', (req, res, next) => {
 	console.log('Data sent');
 });
 
-app.post('/newItem', (req, res) => {
-	return res.send(`POST /newItem: request on port ${PORT}`);
-});
+// app.post('/newItem', (req, res) => {
+// 	return res.send(`POST /newItem: request on port ${PORT}`);
+// });
 
-app.put('/item', (req, res) => {
-	return res.send(`PUT /item: request on port ${PORT}`);
-});
+// app.put('/item', (req, res) => {
+// 	return res.send(`PUT /item: request on port ${PORT}`);
+// });
 
-app.delete('/item', (req, res) => {
-	return res.send(`DELETE /item: request on port ${PORT}`);
-});
+// app.delete('/item', (req, res) => {
+// 	return res.send(`DELETE /item: request on port ${PORT}`);
+// });
+
+app.route('/item')
+	.get((req, res,) => {
+			return res.send(`GET /item: request on port ${PORT}`);
+	})
+	.post((req, res) => {
+		return res.send(`POST /newItem: request on port ${PORT}`);
+	})
+	.put((req, res) => {
+		return res.send(`PUT /item: request on port ${PORT}`);
+	})
+	.delete((req, res) => {
+		return res.send(`DELETE /item: request on port ${PORT}`);
+	});
 
 // serve
 app.listen(PORT, () => {
