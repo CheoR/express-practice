bec7afe9a61de2f00db1f7a8461073b5f7f2d9fd
CheoR <cheo.roman+github@gmail.com>
2021-07-24 21:38:08 -0500

    build: add mock data.

--- index.jsx
+++ index.jsx
@@ -1,8 +1,10 @@
 import express from 'express';
+import data from './data/data.json';
 
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT} . . . `);
+	console.log(data);
 });
